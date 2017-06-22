cd8b4e4911b1f6c949d10f6635c11f3140705e46
Cal Crompton <ccrompton@zynga.com>
2017-06-22 16:46:21 -0700

    Ball Killer Class and some changes to the walls prefab for an Ipad on its side

---
+++ Assets/Scripts/BallKiller.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallKiller : MonoBehaviour {
+
+	public float FirstBallX;
+	public bool FirstBallFound = false;
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnCollisionEnter2D(Collision2D coll) {
+		Debug.Log("collision");
+		if(coll.gameObject.GetComponent<Ball>()){
+			if(!FirstBallFound){
+				FirstBallX = coll.gameObject.transform.position.x;
+				FirstBallFound = true;
+			}
+			Destroy(coll.gameObject);
+		}
+	}
+}
