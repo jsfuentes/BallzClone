4d8a7882208d23a22c88f2567c7f493c93177b08
Samuel Sintz <ssintz@zynga.com>
2017-06-22 11:17:28 -0700

    setting up foundation for project

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Singleton class for the game manager. This should be the central 
+/// point to access everything we need.
+/// </summary>
+public class GameManager : MonoBehaviour {
+
+    public static GameManager instance = null;
+
+    // Property for Player Ball
+
+    // Property for ShapeManager
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        //make sure this is not destoyed on scene reload or change
+        DontDestroyOnLoad(gameObject);
+        this.Init();
+    }
+
+    private void Init()
+    {
+        // level setup here
+    }
+}
---
+++ Assets/Scripts/ShapeManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+/// <summary>
+/// Shape manager. This class works to manage all the blocks balls can hit
+/// This class handles spawning and keeps a reference to all blocks
+/// </summary>
+public class ShapeManager : MonoBehaviour {
+
+    // Add List<Shape> here
+
+	void Start () 
+    {
+	
+	}
+	
+	void Update () 
+    {
+	
+	}
+
+    public void SpawnShapes()
+    {
+
+    }
+}
